10da85d786f2751336b626c27885e9de1cc3e2cd
Amos Wong <amoswwj@gmail.com>
2020-07-04 21:52:04 +0800

    refactor: Move QrReader into its own component

---
+++ src/components/QrReader.vue
@@ -0,0 +1,104 @@
+<template>
+  <canvas
+    v-show="false"
+    class="canvas"
+    ref="canvas"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import jsQr, { QRCode } from 'jsqr'
+
+interface ComponentData {
+  data: QRCode | null;
+}
+
+export default Vue.extend({
+  props: {
+    videoElement: {
+      type: HTMLVideoElement,
+      default: null,
+    },
+    scanInterval: {
+      type: Number,
+      default: 500,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data(): ComponentData {
+    return {
+      data: null,
+    };
+  },
+
+  computed: {
+    canScan(): boolean {
+      return !this.disabled && !!this.videoElement;
+    }
+  },
+
+  watch: {
+    data(newData) {
+      this.$emit('change', newData);
+    },
+
+    videoElement(videoElement: HTMLVideoElement | null): void {
+      if (!videoElement) return;
+
+      this.setupCanvas(videoElement);
+    },
+  },
+
+  methods: {
+    coordinateScanning(ctx: CanvasRenderingContext2D, videoElement: HTMLVideoElement) {
+      ctx.drawImage(videoElement, 0, 0, videoElement.videoWidth, videoElement.videoHeight);
+      const imageData = ctx.getImageData(0, 0, videoElement.videoWidth, videoElement.videoHeight);
+
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const code = jsQr(imageData.data, videoElement.videoWidth, videoElement.videoHeight);
+          if (code && code.data) {
+            this.data = code;
+          }
+          resolve();
+        })
+      })
+    },
+
+    async setupCanvas(videoElement: HTMLVideoElement) {
+      const canvasEl = (this.$refs.canvas as HTMLCanvasElement);
+      const ctx = canvasEl.getContext('2d');
+      if (!ctx) {
+        return;
+      }
+
+      ctx.canvas.width = videoElement.videoWidth;
+      ctx.canvas.height = videoElement.videoHeight;
+      ctx.clearRect(0, 0, videoElement.videoWidth, videoElement.videoHeight);
+
+      const wait = async (duration: number) => new Promise(r => setTimeout(r, duration));
+
+      while(this.canScan) {
+        const start = performance.now();
+        await this.coordinateScanning(ctx, videoElement);
+        const diff = performance.now() - start;
+
+        if (diff < this.scanInterval) {
+          await wait(diff);
+        }
+      }
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.canvas {
+  width: 100%;
+}
+</style>
--- src/components/qr-scanner.vue
+++ src/components/qr-scanner.vue
@@ -3,12 +3,12 @@
     <CameraFeed
       class="video"
       autoplay
-      @ready="setupCanvas"
+      @ready="cameraFeedVideoElement = $event"
     />
-    <canvas
-      v-show="false"
-      class="canvas"
-      ref="canvas"
+    <QrReader
+      :disabled="disabled"
+      :videoElement="cameraFeedVideoElement"
+      @change="data = $event"
     />
     <ContentRenderer
       v-if="data"
@@ -20,90 +20,40 @@
 <script lang="ts">
 import Vue from 'vue';
 import ContentRenderer from './ContentRenderer.vue'
+import QrReader from './QrReader.vue'
 import CameraFeed from './camera-feed.vue'
-import jsQr, { QRCode } from 'jsqr'
+import { QRCode } from 'jsqr';
 
 interface ComponentData {
   data: QRCode | null;
-  disabled: boolean;
+  cameraFeedVideoElement: HTMLVideoElement | null;
 }
 
 export default Vue.extend({
   components: {
     ContentRenderer,
+    QrReader,
     CameraFeed,
   },
 
   props: {
-    scanInterval: {
-      type: Number,
-      default: 500,
+    disabled: {
+      type: Boolean,
+      default: false,
     },
   },
 
   data(): ComponentData {
     return {
       data: null,
-      disabled: false,
+      cameraFeedVideoElement: null,
     };
-  },
-
-  watch: {
-    data(newData) {
-      this.$emit('change', newData);
-    },
-  },
-
-  methods: {
-    coordinateScanning(ctx: CanvasRenderingContext2D, videoElement: HTMLVideoElement) {
-      ctx.drawImage(videoElement, 0, 0, videoElement.videoWidth, videoElement.videoHeight);
-      const imageData = ctx.getImageData(0, 0, videoElement.videoWidth, videoElement.videoHeight);
-
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          const code = jsQr(imageData.data, videoElement.videoWidth, videoElement.videoHeight);
-          if (code && code.data) {
-            this.data = code;
-          }
-          resolve();
-        })
-      })
-    },
-
-    async setupCanvas(videoElement: HTMLVideoElement) {
-      const canvasEl = (this.$refs.canvas as HTMLCanvasElement);
-      const ctx = canvasEl.getContext('2d');
-      if (!ctx) {
-        return;
-      }
-
-      ctx.canvas.width = videoElement.videoWidth;
-      ctx.canvas.height = videoElement.videoHeight;
-      ctx.clearRect(0, 0, videoElement.videoWidth, videoElement.videoHeight);
-
-      const wait = async (duration: number) => new Promise(r => setTimeout(r, duration));
-
-      while(!this.disabled) {
-        console.log(performance.now());
-        const start = performance.now();
-        await this.coordinateScanning(ctx, videoElement);
-        const diff = performance.now() - start;
-
-        if (diff < this.scanInterval) {
-          await wait(diff);
-        }
-      }
-    },
   },
 });
 </script>
 
 <style scoped lang="scss">
 .video {
-  width: 100%;
-}
-
-.canvas {
   width: 100%;
 }
 </style>
