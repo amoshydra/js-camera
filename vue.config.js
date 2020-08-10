0b33307739d85fb7495e5c50167bf774259bdc75
Amos Wong <amoswwj@gmail.com>
2020-08-10 23:27:34 +0800

    feat: Configure application name

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,24 @@
+const appTitle = 'QR Camera';
+
 // vue.config.js
 module.exports = {
   // options...
   devServer: {
     disableHostCheck: true
+  },
+
+  pwa: {
+    name: appTitle,
+  },
+
+  chainWebpack: config => {
+
+    config
+      .plugin('html')
+      .tap(([ htmllWebpackPluginOption ]) => {
+
+        htmllWebpackPluginOption.title = appTitle;
+        return [ htmllWebpackPluginOption ]
+      })
   }
 }
