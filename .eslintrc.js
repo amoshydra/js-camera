e0948b3aa93a36b93e9baf8413a7521fe0ec59dc
Amos Wong <amoswwj@gmail.com>
2021-11-14 01:30:32 +0800

    chore(lint): Turn off incompatible no-undef rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,10 @@ module.exports = {
   rules: {
     'no-console': 'warn',
     'no-debugger': 'warn',
+
+    // This rule is not compatible with typescript-eslint-parser
+    // See: https://github.com/eslint/typescript-eslint-parser/issues/437
+    'no-undef': 'off',
     // This issue is not compatible with Generic used in .vue file
     // See: https://github.com/typescript-eslint/typescript-eslint/issues/4062
     "@typescript-eslint/no-unnecessary-type-constraint": "off",
