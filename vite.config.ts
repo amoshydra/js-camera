cf9df2947ebd1a51b70c587c8e076f8b5908e938
amoshydra <8733840+amoshydra@users.noreply.github.com>
2026-03-08 07:55:52 +0800

    feat: configura pwa manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'QR Camera',
+        display: 'standalone',
+        background_color: '#080808',
+        theme_color: '#080808',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
