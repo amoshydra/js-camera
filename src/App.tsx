cd4fe5503fb83e12741d5f0dde1c12b0b2013de5
amoshydra <8733840+amoshydra@users.noreply.github.com>
2026-03-08 03:47:37 +0800

    fix: remove redundant id, move global style to html

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import { css } from '~styled-system/css';
 import { useDocumentVisibilityChange } from './hooks/useVisibilityChange';
 import QrScanner from './pages/QrScanner';
-
-const globalStyles = css({
-  fontFamily: 'Avenir, Helvetica, Arial, sans-serif',
-  fontSmoothing: 'antialiased',
-  color: '#2c3e50',
-});
 
 export default function App() {
   const documentVisible = useDocumentVisibilityChange();
 
   return (
-    <div
-      className={globalStyles}
-      id="app"
-    >
-      <QrScanner disabled={documentVisible} />
-    </div>
+    <QrScanner disabled={documentVisible} />
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '../styled-system/styles.css';
+import '~styled-system/styles.css';
 import App from './App';
 
 createRoot(document.getElementById('app')!).render(
