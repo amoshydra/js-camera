310ceca88f3a102158cd3c73c29d920c0c332848
amoshydra <8733840+amoshydra@users.noreply.github.com>
2026-03-09 01:24:06 +0800

    fix: update favicon and icons for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,36 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
+const base = process.env.BUILD_PUBLIC_PATH ?? '/';
+
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       manifest: {
         name: 'QR Camera',
-        display: 'standalone',
+        description: 'A QR code scanner in the browser',
+        id: 'https://amoshydra.github.io/js-camera/',
+        start_url: 'https://amoshydra.github.io/js-camera/',
+        display: 'fullscreen',
+        categories: ['utilities'],
         background_color: '#080808',
         theme_color: '#080808',
+        icons: [
+          {
+            src: base + 'img/icons/square-128.png',
+            sizes: '128x128',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: base + 'img/icons/square-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        screenshots: [],
       },
     }),
   ],
@@ -21,7 +42,7 @@ export default defineConfig({
       '~styled-system': resolve(__dirname, 'styled-system'),
     },
   },
-  base: process.env.BUILD_PUBLIC_PATH,
+  base,
   server: {
     host: true,
     allowedHosts: true,
